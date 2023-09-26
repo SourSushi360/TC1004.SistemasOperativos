4f7ad469dfd78553d6c7b5bccf2d66588d5bf881
SourSushi360 <jplopez365@hotmail.com>
2023-09-26 08:33:45 -0600

    branch completition

---
+++ main.c
@@ -0,0 +1,10 @@
+/*  main.cpp
+    Jean Paul López A01637266
+    
+    Un código de practica para los branch */
+#include <stdio.h>
+int main() {
+    printf("hello world\n");
+    
+    return 1;
+}
