e560841a844bbc6537551795461e76d13ac302a9
SourSushi360 <jplopez365@hotmail.com>
2023-10-03 08:15:10 -0600

    probar el fork

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 /*  main.cpp
     Jean Paul López A01637266
     
-    Un código de practica para los branch */
+    Un código de practica para la creacion de apps */
 #include <stdio.h>
+#include <unistd.h>
 int main() {
     printf("hello world\n");
+    fork();
     
     return 1;
 }
